462400b98b41518fa1db95e485691e418c5c3c13
Praveen <sankar_praveen@hotmail.com>
2021-07-23 15:47:02 +0530

    accesskey added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,7 +16,7 @@ export class AppComponent implements OnInit{
   }
 
   ngOnInit(){  
-    this.httpService.GetLocation().subscribe(res=> this.locData = res);
+    this.httpService.GetLocation().subscribe(res=> {this.locData = res; console.log(res)});
   }
 
   OnClick(){
