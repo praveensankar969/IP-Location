8c725c0a79cd00aa6c41cde424107ae82eb10324
Praveen <sankar_praveen@hotmail.com>
2021-07-23 15:26:26 +0530

    upgrade insecure request

--- src/app/http.service.ts
+++ src/app/http.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { GeoInterface } from './GeoInterface';
 import {map, shareReplay} from 'rxjs/operators'
@@ -18,7 +18,9 @@ export class HttpService {
   }
 
   CallApi(){
-    this.obs = this.http.get<GeoInterface>("http://api.ipstack.com/check?access_key=e6d5204a6d88b1cf7041817ef0b3e34c").pipe(map(res=> {console.log(res); return res;}),shareReplay());
+    let param  = new HttpParams();
+    param.append("access_key", "e6d5204a6d88b1cf7041817ef0b3e34c")
+    this.obs = this.http.get<GeoInterface>("http://api.ipstack.com/check", {params : param}).pipe(map(res=> {console.log(res); return res;}),shareReplay());
   }
 
   
