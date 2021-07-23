890d6b6a8b87682919cd3a5bce6ff4cd5d9fea7f
Praveen <sankar_praveen@hotmail.com>
2021-07-23 15:35:34 +0530

    removed share replay

--- src/app/http.service.ts
+++ src/app/http.service.ts
@@ -20,7 +20,7 @@ export class HttpService {
   CallApi(){
     let param  = new HttpParams();
     param.append("access_key", "e6d5204a6d88b1cf7041817ef0b3e34c")
-    this.obs = this.http.get<GeoInterface>("http://api.ipstack.com/check", {params : param}).pipe(map(res=> {console.log(res); return res;}),shareReplay());
+    this.obs = this.http.get<GeoInterface>("http://api.ipstack.com/check", {params : param}).pipe(map(res=> {console.log(res); return res;}));
   }
 
   
